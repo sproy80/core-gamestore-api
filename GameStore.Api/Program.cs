fe188073f7544c1c89bbfa196ca08fd7e588fe2e
Sanjay Prosad Roy <roysanjay3@gmail.com>
2024-06-05 08:50:05 +0530

    Entities and Context class added. Required ef tool installed

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,15 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
+
 var app = builder.Build();
+
 
 app.MapGamesEndPoints();
 
